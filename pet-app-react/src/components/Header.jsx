54f4f59d07d5f6c1d10390fd23da318b825523ac
Alex <alex@Alexs-MacBook-Air.local>
2025-03-18 20:30:53 -0500

    style updates, added dummy text to profile for now

--- pet-app-react/src/components/Header.jsx
+++ pet-app-react/src/components/Header.jsx
@@ -5,11 +5,16 @@ export default function Header({ jwt }) {
         <header className="navbar">
             <nav>
                 <NavLink className="nav-item" to="/">Home</NavLink>
-                <NavLink className="nav-item" to="/login">Login</NavLink>
-                <NavLink className="nav-item" to="/register" >Register</NavLink>
                 {jwt !== "" && 
                 <NavLink className="nav-item" to="/profile">Profile</NavLink>
                 }
+                {jwt === "" &&
+                    <NavLink className="nav-item" to="/login">Login</NavLink>
+                }
+                {jwt !== "" &&
+                    <NavLink className="nav-item">Logout</NavLink>
+                }
+                {/* <NavLink className="nav-item" to="/register" >Register</NavLink> */}
             </nav>
         </header>
 
