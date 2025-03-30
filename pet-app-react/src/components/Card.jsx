87f7d7ad9cb8ee436d1fabf9c125b51808026231
Alex <alex@Alexs-MacBook-Air.local>
2025-03-30 01:45:55 -0500

    Major style revamp, pet cards looking pretty nice and uniform

--- pet-app-react/src/components/Card.jsx
+++ pet-app-react/src/components/Card.jsx
@@ -4,7 +4,7 @@ export default function Card({ pet }) {
 
     function handleNullPhoto(photo) {
         if (photo === null) {
-            return "https://t3.ftcdn.net/jpg/05/34/21/24/360_F_534212408_moxhV1d5Xj0TJiUDbnJvZmZrxmdDXH71.jpg"
+            return "/src/assets/pet-placeholder-img.jpg"
         }
         else {
             return photo.full;
@@ -14,9 +14,8 @@ export default function Card({ pet }) {
     return (
         <a href={pet.url} target="_blank">
             <div key={pet.id} className="card">
+                <img className="pet-photo" src={handleNullPhoto(pet.primary_photo_cropped)} alt={`Pet photo for a ${pet.type} named ${pet.name}`} />
                 <h4 className="pet-name">{pet.name}</h4>
-                <img className="pet-photo" src={handleNullPhoto(pet.primary_photo_cropped)} alt="pet photo" />
-                {/* <h4>{pet.type}</h4> */}
             </div>
         </a>
     );
